c005ec8229da39658bb275a3fd760b08b0341d29
Alexander Zvenigorodsky <teckerpro@gmail.com>
2022-11-11 21:36:51 +0400

    Added pushing, templating, restarting

--- src/viewer.go
+++ src/viewer.go
@@ -5,24 +5,32 @@ import (
     "fmt"
     "log"
     "net/http"
-	"os/exec"
+ "os/exec"
 )
-
+var conCount int
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	cmd := exec.Command("/opt/test/executor")
-	stdout, err := cmd.Output()
+    cmd := exec.Command("/opt/test/executor")
+    stdout, err := cmd.Output()
 
     if err != nil {
         fmt.Println(err.Error())
         return
     }
-	res := string(stdout)
+if conCount > 0 {
+ res := string(stdout)
     fmt.Println(res)
     fmt.Fprintf(w, res)
+    if r.URL.Path == "/" {
+    conCount -= 1
+    }
+    } else {
+        w.WriteHeader(500)
+    }
 }
 
 
 func main() {
+    conCount = 10
     http.HandleFunc("/", helloHandler) // Update this line of code
 
 
